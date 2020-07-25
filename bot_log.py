e5577175a05c40344c70601e6cf80f0af84e39ff
Genomorf <55433916+Genomorf@users.noreply.github.com>
2020-07-25 19:41:44 +0300

    Update bot_log.py

--- bot_log.py
+++ bot_log.py
@@ -1,10 +1,19 @@
 import telebot
 import subprocess
-
 bot = telebot.TeleBot("1158344914:AAELM03sIzd9b-pWnA0pbr7V2ForhmwbRSc")
+
+
 
 @bot.message_handler(commands=["log"])
 def send_welcome(message):
-        result = subprocess.run(['journalctl', '-u', 'tgbot.service'], stdout=subprocess.PIPE, text=True)
-        bot.reply_to(message, result)
+
+    result = subprocess.run(['journalctl', '-u', 'tgbot.service'], stdout=subprocess.PIPE)
+    if result:
+        bot.reply_to(message, result.stdout)
+    else:
+        bot.reply_to(message, "No logs")
+
+
 bot.polling()
+
+
