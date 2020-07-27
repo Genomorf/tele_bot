71d9778efa6ba77660c447b65eb9151d471daac7
Genomorf <55433916+Genomorf@users.noreply.github.com>
2020-07-27 13:03:04 +0300

    Update bot_log.py

--- bot_log.py
+++ bot_log.py
@@ -1,42 +1,31 @@
 import telebot
 import subprocess
-
+from telebot import util
 bot = telebot.TeleBot("1158344914:AAELM03sIzd9b-pWnA0pbr7V2ForhmwbRSc")
 
 
-def send_to_log(message, log_name):
-    log=''
-    with open(f'/home/gena/tele_bot/logs/{log_name}', 'r') as F:
-        for string in F:
-            log += string
-    for i in range(0, len(log), 200):
-        if len(log) - i < 200:
-            bot.reply_to(message, log[i:len(log)])
-        else:
-            bot.reply_to(message, log[i:i+100])
 
+def send_to_log(message1, log_name):
+    large_text = open(f'/home/gena/tele_bot/logs/{log_name}', "rb").read()
+    splitted_text = util.split_string(large_text, 3000)
+    for text in splitted_text:
+	    bot.reply_to(message1, "start: ' + str(text))
 
-@bot.message_handler(commands=["sd_log"])
-def send_welcome(message):
-    send_to_log(message, "app_sd.log")
-
-
-@bot.message_handler(commands=["err_log"])
-def send_welcome(message):
-    send_to_log(message, "ap_err.log")
 
 
 @bot.message_handler(commands=["log"])
 def send_welcome(message):
     send_to_log(message, "bot.log")
 
+@bot.message_handler(commands=["restart"])
+def restart_bot(message):
+    res = subprocess.run(["sudo", "systemctl","restart","tgbot"], stdout=subprocess.PIPE)
+    res1 = 'stdout: '
+    bot.reply_to(message,res1 + str(res.stdout))
 
-@bot.message_handler(commands=["process"])
-def send_welcome(message):
-    result = subprocess.run(['superviserctl', 'status', 'tele_bot'], stdout=subprocess.PIPE)
-    bot.reply_to(message, result.stdout)
-
-
+@bot.message_handler(commands=["status"])
+def status_bot(message):
+    res = subprocess.run(["sudo", "systemctl", "status", "tgbot"], stdout=subprocess.PIPE)
+    res1 = 'status: '
+    bot.reply_to(message,res1 + str(res.stdout))
 bot.polling()
-
-
