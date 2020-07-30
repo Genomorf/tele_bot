4434ce06de49a634fd0c859880203593e1d403ca
Genomorf <55433916+Genomorf@users.noreply.github.com>
2020-07-30 21:27:51 +0300

    Create log_module.py

---
+++ log_module.py
@@ -0,0 +1,7 @@
+import logging
+
+# logger 
+logging.basicConfig(format=u'%(filename)s[LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s]  %(message)s',
+                    filename='/home/gena/tele_bot/logs/bot.log', level=logging.INFO)
+           
+
